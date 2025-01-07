58173a63aecf9d465ca10da8ae1d5fb0b5dd6e05
arnabtm <arnab@trademarkia.com>
2025-01-08 01:11:23 +0530

    update conflcits

--- app/(app)/(transporter)/searchTrucks.js
+++ app/(app)/(transporter)/searchTrucks.js
@@ -4,6 +4,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import LoadingPoint from "../../../assets/images/icons/LoadingPoint";
 import { useAuth } from "../../../context/AuthProvider";
 import FormInput from "../../../components/FormInput";
+import TruckCard from "../../../components/TruckCard";
 
 const SearchTrucks = () => {
   const { colour } = useAuth();
@@ -11,6 +12,22 @@ const SearchTrucks = () => {
   return (
     <SafeAreaView style={{ paddingHorizontal: 20, paddingVertical: 10 }}>
       <FormInput Icon={LoadingPoint} placeholder='Enter origin' name='origin' />
+      <TruckCard
+        data={{
+          source: "Lagos",
+          destination: "Abuja",
+          weight: 2000,
+          offeredAmount: 500000,
+          numberOfWheels: 10,
+          vehicleType: "Truck",
+          vehicleBodyType: "Open",
+          bids: 5,
+          views: 100,
+          expiresAt: "2022-12-12",
+          tripDistance: 1000,
+          advanceAmount: 100000,
+        }}
+      />
     </SafeAreaView>
   );
 };
